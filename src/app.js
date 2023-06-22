d37579a95bd50d86a3766dfa9e7ca43eb28cb93f
bruno <brunoqueiroz612@gmail.com>
2023-06-22 08:25:56 -0300

    Initial commit.

---
+++ src/app.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+const app = express();
+dotenv.config();
+
+const port = process.env.PORT || 3000;
+
+const allowedUrls = [
+  "http://localhost:5173",
+  "http://localhost:3000",
+  "http://localhost:5174",
+];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedUrls.indexOf(origin || "") !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(cookieParser());
+
+app.get("/add-cookie", (req, res) => {
+  console.log("testing route add");
+
+  res.cookie("cookie-test", "test values", {
+    httpOnly: true,
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 1 week
+  });
+
+  res.json({ msg: "cookie added" });
+});
+
+app.get("/remove-cookie", (req, res) => {
+  console.log("testing route remove");
+
+  res.clearCookie("cookie-test");
+
+  res.json({ msg: "cookie removed" });
+});
+
+app.listen(port, () => console.log("runninggggg"));
